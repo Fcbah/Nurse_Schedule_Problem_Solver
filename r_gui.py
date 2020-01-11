9ec89e571f531af6f11096c62778e1beeac9884a
Akinleye Hephzibah B <fcbah1248@gmail.com>
2020-01-11 22:47:23 +0100

    this is the one that worked with expansion

--- r_gui.py
+++ r_gui.py
@@ -625,15 +625,36 @@ class CanvWrap:
 
 r = Prob.NSP()
 master = Tk()
-f = Frame(master)
-scrx = Scrollbar(f,orient=HORIZONTAL)
-scry = Scrollbar(f,orient=VERTICAL)
 
-gh = CanvWrap(f,r,scrx,scry)
+#f = Frame(master)
+#g = Frame(master)
+#h = Frame(master)
+
+#g1 = Listbox(g)
+#g2 = Listbox(g)
+#h1 = Listbox(h)
+
+#g1.pack(side=TOP,expand=NO,fill=X)
+#g2.pack(side=TOP,expand=NO,fill=X)
+#h1.pack(side=TOP, expand=NO, fill=X)
+
+scrx = Scrollbar(master,orient=HORIZONTAL)
+scry = Scrollbar(master,orient=VERTICAL)
+gh = CanvWrap(master,r,scrx,scry)
+
+#scrx.grid(row=1,column=0,sticky=EW)
+#scry.grid(row=0,column=1,sticky= NS)
+#gh.canvas.grid(row=0,column=0, sticky=NSEW)
+#gh.canvas.rowconfigure(0,weight=1)
+#gh.canvas.columnconfigure(0,weight=1)
+
+#g.pack(side=LEFT,expand=NO,fill=Y)
+#f.pack(side=LEFT, expand=YES, fill=BOTH)
+#h.pack(side=LEFT, expand=NO, fill=Y)
 
 scrx.pack(side=TOP,expand=NO,fill=X)
 scry.pack(side=LEFT,expand=NO,fill=Y)
-gh.canvas.pack(side=RIGHT,expand=YES, fill=BOTH)
+gh.canvas.pack(side=LEFT,expand=YES,fill=BOTH)
 
 p =r.particles.copy().popitem()[1]
 
